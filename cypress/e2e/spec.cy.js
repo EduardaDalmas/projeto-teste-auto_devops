54575a077b7bc3157b0bf9799ab0e9a65a10cd4b
EduardaDalmas <eduarda.dalmas@doctorclin.com.br>
2023-11-23 11:16:25 -0300

    criados testes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,36 +1,51 @@
 describe('Acesso ao sistema de teste', () => {
 	beforeEach(() => {
-		cy.visit('https://www.saucedemo.com/');
+		cy.visit('https://projetoweb1hoteljpand.000webhostapp.com/index.php/');
 	});
 
-	it('Validar a tela de login', () => {
-		cy.title().should('eq', 'Swag Labs');
+	it('Validar a página', () => {
+		cy.title().should('eq', 'Hotelaria do Diego - Home');
 	});
 
-	it('Preencher o formulário de login', () => {
-		cy.get('#user-name').type('standard_user');
-		cy.get('#password').type('secret_sauce');
-		cy.get('#login-button').click();
-		cy.get('.app_logo').should('contain', 'Swag Labs');
+	it('Testes', () => {
+		// Validar se possui quartos disponíveis
+		cy.get('.card').should('have.length', 1);
+		cy.get('.btn-primary').should('contain', 'Ver mais quartos').click();
+		cy.get('.quarto').should('have.length', 1);
 
-		cy.get('.product_sort_container').select('Price (high to low)');
-		cy.get('.inventory_item_price').first().should('contain', '$49.99');
-		cy.get('.inventory_item_name')
-			.first()
-			.should('contain', 'Sauce Labs Fleece Jacket')
-			.click();
-		cy.get('.btn_primary.btn_inventory').click();
-		cy.get('.shopping_cart_link').click();
-		cy.get('.cart_item').should('have.length', 1);
-		cy.get('.cart_item').should('contain', 'Sauce Labs Fleece Jacket');
-		cy.get('.btn_action.checkout_button').click();
-		cy.get('#first-name').type('Teste');
-		cy.get('#last-name').type('Teste');
-		cy.get('#postal-code').type('00000-000');
-		cy.get('.btn_primary.cart_button').click();
-		cy.get('#finish').click();
-		cy.get('.complete-header').should('contain', 'Thank you for your order!');
-		cy.get('.shopping_cart_link').click();
-		cy.get('.cart_item').should('have.length', 0);
+		// Reservar quarto
+		cy.get('.btn-primary').should('contain', 'Detalhes').click();
+		cy.get('.btn-primary').should('contain', 'Reservar').click();
+		cy.get('.btn-primary').should('contain', 'Efetuar Reserva').click();
+		cy.get('#validationCustom01').type('Eduarda Dalmas');
+		cy.get('#validationCustom02').type('eduardadalmas@ienh.com.br');
+		cy.get('#validationCustomUsername').type('05/01/2024');
+		cy.get('#quarto').first().should('contain', '123');
+		cy.get('#validationCustom05').type('1');
+		cy.get('#invalidCheck').click();
+		cy.get('.btn-primary').should('contain', 'Fazer Reserva').click();
+		cy.visit('https://projetoweb1hoteljpand.000webhostapp.com/index.php/');
+
+		// Incluir quarto no admin
+		cy.get('.btn-primary').should('contain', 'Painel admin').click();
+		cy.get('.btn-primary').should('contain', 'Criar quartos').click();
+		cy.get('#quarto').type('NY - vista para o Central Park');
+		cy.get('#descricao').type('Quarto com vista para o Central Park');
+		cy.get('#valor').type('R$5.000,00 dia');
+		cy.get('#imagem').click().first().click();
+		cy.get('.btn-primary').should('contain', 'Adicionar quarto').click();
+
+		// Editar quarto no admin
+		cy.get('.btn-primary').should('contain', 'Painel admin').click();
+		cy.get('.btn-primary').should('contain', 'Visualizar quartos').click();
+		cy.get('.btn-primary').should('contain', 'Editar Quarto').first().click();
+		cy.get('#descricao').type('Quarto com vista para as Maldivas');
+		cy.get('#valor').type('R$10.000,00 dia');
+		cy.get('.btn-primary').should('contain', 'Salvar').click();
+
+		// Excluir quarto no admin
+		cy.get('.btn-primary').should('contain', 'Painel admin').click();
+		cy.get('.btn-primary').should('contain', 'Visualizar quartos').click();
+		cy.get('.btn-primary').should('contain', 'Excluir Quarto').first().click();
 	});
 });
